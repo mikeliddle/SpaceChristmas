3366472f6b4be06e52094561688243d1457ff528
Mike Liddle <miliddle@microsoft.com>
2019-11-02 20:15:56 -0600

    Add project files.

---
+++ SpaceChristmas/Data/EventContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SpaceChristmas.Models
+{
+    public class EventContext : DbContext
+    {
+        public EventContext (DbContextOptions<EventContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SpaceChristmas.Models.Event> Event { get; set; }
+    }
+}
---
+++ SpaceChristmas/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaceChristmas.Models
+{
+    public class Event
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int SequenceNumber { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Name { get; set; }
+        public string Result { get; set; }
+    }
+}
