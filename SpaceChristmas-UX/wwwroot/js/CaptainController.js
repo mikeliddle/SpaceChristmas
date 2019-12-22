14a9f3e2f3af2310c835b3b5376278ea7c701884
Michael Liddle <miliddle@microsoft.com>
2019-12-21 21:49:37 -0700

    updated captain page

--- SpaceChristmas-UX/wwwroot/js/CaptainController.js
+++ SpaceChristmas-UX/wwwroot/js/CaptainController.js
@@ -29,6 +29,28 @@ function tearDownView() {
     document.getElementById("gameCanvas").innerHTML = "";
 }
 
+function firstLoad() {
+    document.getElementById("gameCanvas").innerHTML = '<table class="captainControls">' +
+        '<tr><td>' +
+                '<h1 class="text-success" id="AlertColor">Alert Status: <span id="AlertText">1</span></h1>' +
+            '</td></tr><tr><td></td></tr>' +
+        '<tr><td>' +
+                '<button class="btn btn-outline-success" onclick="document.getElementById(\'AlertColor\').className = \'text-success\'; document.getElementById(\'AlertText\').innerHTML = 1;">Alert 1!</button>' +
+            '</td></tr>' +
+        '<tr><td>' +
+                '<button class="btn btn-outline-primary" onclick="document.getElementById(\'AlertColor\').className = \'text-primary\'; document.getElementById(\'AlertText\').innerHTML = 2;">Alert 2!</button>' +
+            '</td></tr>' +
+        '<tr><td>' +
+                '<button class="btn btn-outline-warning" onclick="document.getElementById(\'AlertColor\').className = \'text-warning\'; document.getElementById(\'AlertText\').innerHTML = 3;">Alert 3!</button>' +
+            '</td></tr>' +
+        '<tr><td>' +
+                '<button class="btn btn-outline-danger" onclick="document.getElementById(\'AlertColor\').className = \'text-danger\'; document.getElementById(\'AlertText\').innerHTML = 4; document.getElementById(\'battleStationsButton\').disabled = false;">Alert 4!</button>' +
+            '</td><td>' +
+                '<button id="battleStationsButton" class="btn btn-outline-danger" onclick="alertBattleStations()" disabled>Battle Stations!</button>' +
+            '</td></tr>' +
+        '</table>';
+}
+
 function eventLoop() {
     if (eventQueue.length > 0) {
         var event = eventQueue[0];
@@ -64,7 +86,7 @@ function eventLoop() {
             tearDownView();
             firstLoad();
         } else if (event.Name === "firstLoad") {
-            tearDownView();
+            //tearDownView();
             firstLoad();
         }
     }
